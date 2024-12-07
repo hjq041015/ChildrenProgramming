0f7d6e745db755cb4418d8a070334d57b27bc97c
zhiyi <huangjiaqi041015@gmail.com>
2024-12-07 15:45:22 +0800

    完成首页数据以及导航栏的编写

---
+++ src/components/BackToTop.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {onMounted} from "vue";
+import {useBactToTop} from "@/utils/common.js";
+
+onMounted(useBactToTop)
+</script>
+
+<template>
+  <div id="scrollToTop" class="scrolltop-show">
+    <i class="icon-right-arrow-11"></i>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
